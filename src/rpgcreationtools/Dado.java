97c3543a26c4e0b24e682acca21865aae22744ad
DavidGomes7 <j.davidgms.dg@gmail.com>
2020-11-07 23:09:01 -0300

    Added d100 option

--- src/rpgcreationtools/Dado.java
+++ src/rpgcreationtools/Dado.java
@@ -17,40 +17,46 @@ public class Dado {
     Random roll = new Random();
     
     // d20
-    public static void rolarD20(){
+    public static void rolarD20() {
         double roll = random() * 2;
         roll = Math.round(roll * 10);
         JOptionPane.showMessageDialog(null, roll, "D20", JOptionPane.PLAIN_MESSAGE);
     }
     
     // d4
-    public static void rolarD4(){
+    public static void rolarD4() {
         Random roll = new Random();
         JOptionPane.showMessageDialog(null, roll.nextInt(4) + 1, "D4", JOptionPane.PLAIN_MESSAGE);
     }
     
     // d6
-    public static void rolarD6(){
+    public static void rolarD6() {
         Random roll = new Random();
         JOptionPane.showMessageDialog(null, roll.nextInt(6) + 1, "D6", JOptionPane.PLAIN_MESSAGE);
     }
     
     // d8
-    public static void rolarD8(){
+    public static void rolarD8() {
         Random roll = new Random();
         JOptionPane.showMessageDialog(null, roll.nextInt(8) + 1, "D8", JOptionPane.PLAIN_MESSAGE);
     }
     
     // d10
-    public static void rolarD10(){
+    public static void rolarD10() {
         Random roll = new Random();
         JOptionPane.showMessageDialog(null, roll.nextInt(10) + 1, "D10", JOptionPane.PLAIN_MESSAGE);
     }
     
     // d12  
-    public static void rolarD12(){
+    public static void rolarD12() {
         Random roll = new Random();
         JOptionPane.showMessageDialog(null, roll.nextInt(12) + 1, "D12", JOptionPane.PLAIN_MESSAGE);
+    }
+    
+    // d100
+    public static void rolarD100() {
+        Random roll = new Random();
+        JOptionPane.showMessageDialog(null, roll.nextInt(100) + 1, "D100", JOptionPane.PLAIN_MESSAGE);
     }
     
 }
